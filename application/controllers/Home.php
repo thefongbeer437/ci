6e727481d24459fe150e02faa618db2795b2c469
nantawit <nantawit001@gmail.com>
2016-11-26 16:34:50 +0700

    update last 4.0

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,16 +1,19 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: lab
- * Date: 26/11/2559
- * Time: 13:44
- */
+
 class Home extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('layout/header_view');
         $this->load->view('Home_View');
+        $this->load->view('layout/footer_View');
     }
 
+    public function test()
+    {
+        $this->load->view('layout/header_view');
+        $this->load->view('layout/test_view');
+        $this->load->view('layout/footer_View');
+    }
 }
---
+++ application/views/layout/test_view.php
@@ -0,0 +1,5 @@
+<div>
+    <p>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias aspernatur culpa distinctio ea eligendi est, illo impedit ipsum laboriosam nulla odit optio possimus quo sapiente tempora voluptas voluptatem voluptates!</span><span>Ducimus, harum incidunt iusto minima natus nihil nostrum, perferendis possimus quae sequi similique temporibus vero voluptatum! Beatae consequuntur distinctio excepturi inventore laboriosam nam nobis optio quas repudiandae, saepe sapiente, veniam!</span><span>Accusantium amet architecto debitis ea eius facilis inventore minima porro quam quas quo, sed similique sint sit tempore ullam veniam. Accusantium at distinctio ducimus laboriosam laborum nostrum nulla suscipit vitae.</span>
+    </p>
+</div>
